c723e4cbd9fe2cb9ae2dde39507ee32758f315a9
Nouredean6 <116735207+Nouredean6@users.noreply.github.com>
2023-11-07 01:51:32 +0100

    Update tour.js

--- src/components/tourr/tour.js
+++ src/components/tourr/tour.js
@@ -1,10 +1,8 @@
 import TourData from "./tourData";
 import "./tourStyle.css";
 import Tour1 from "../../assets/marrakech.jpg";
-// import Tour2 from "../../assets/6.jpg";
 import Tour3 from "../../assets/taghazout2.jpg";
 import Tour4 from "../../assets/chefchaouen1.jpg";
-// import Tour5 from "../../assets/10.jpg";
 import Tour6 from "../../assets/fes.jpeg";
 import { useEffect, useState } from "react";
 // import { getTours, selectTours } from "../../redux/features/tour/tourSlice";
@@ -12,54 +10,155 @@ import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { BACKEND_URL } from "../../utils/react-backend-url";
 import DOMPurify from 'dompurify';
-
+import Skeleton, {SkeletonTheme} from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import tourSkeleton from "./tourSkeleton";
 
 function Tour() {
    const [tours, setTours] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
 
   const getTours = async () => {
-    setIsLoading(true);
+    // setIsLoading(true);
     try {
       const { data } = await axios.get(`${BACKEND_URL}/api/v1/tours`);
       setTours(data);
-
-      setIsLoading(false);
+      // setIsLoading(false);
     } catch (error) {
       // toast.error(error.message);
-      setIsLoading(false);
+      // setIsLoading(false);
     }
   };
 
   useEffect(() => {
     getTours();
   }, []);
-
-
-
-  
   return (
+    <SkeletonTheme baseColor="#c0c0c0" highlightColor="#d0d0d0">
+
     <div className="tour">
       <h1> Recent tours </h1>
       <p>You can discover any destination using google maps</p>
       <div className="tourcard">
+      {/* {isLoading &&(
+        <p>
+        <Skeleton count={10}/>
+        </p>
+      )} */}
+      {isLoading ? (
+  <SkeletonTheme baseColor="#c0c0c0" highlightColor="#d0d0d0">
+    <div className="t-card">
+      <div className="t-image">
+        <Skeleton width={'100%'} height={'100%'}/>
+      </div>
+      <h4><Skeleton/></h4>
+      <p ><Skeleton/></p>
+      <div className="btnlearn">
+    <a><Skeleton/></a>
+      </div>
+</div>
+<div className="t-card">
+    <div className="t-image">
+    <Skeleton width={'100%'} height={'100%'}/>
+    </div>
+   <h4><Skeleton/></h4>
+    <p ><Skeleton/></p>
+  <div className="btnlearn">
+    <a><Skeleton/></a>
+  </div>
+</div>
+<div className="t-card">
+    <div className="t-image">
+    <Skeleton width={'100%'} height={'100%'}/>
+    </div>
+   <h4><Skeleton/></h4>
+    <p ><Skeleton/></p>
+  <div className="btnlearn">
+    <a><Skeleton/></a>
+  </div>
+</div>
+<div className="t-card">
+    <div className="t-image">
+    <Skeleton width={'100%'} height={'100%'}/>
+    </div>
+   <h4><Skeleton/></h4>
+    <p ><Skeleton/></p>
+  <div className="btnlearn">
+    <a><Skeleton/></a>
+  </div>
+</div> 
+<div className="t-card">
+      <div className="t-image">
+        <Skeleton width={'100%'} height={'100%'}/>
+      </div>
+      <h4><Skeleton/></h4>
+      <p ><Skeleton/></p>
+      <div className="btnlearn">
+    <a><Skeleton/></a>
+      </div>
+</div>
+<div className="t-card">
+      <div className="t-image">
+        <Skeleton width={'100%'} height={'100%'}/>
+      </div>
+      <h4><Skeleton/></h4>
+      <p ><Skeleton/></p>
+      <div className="btnlearn">
+    <a><Skeleton/></a>
+      </div>
+</div>
+<div className="t-card">
+      <div className="t-image">
+        <Skeleton width={'100%'} height={'100%'}/>
+      </div>
+      <h4><Skeleton/></h4>
+      <p ><Skeleton/></p>
+      <div className="btnlearn">
+    <a><Skeleton/></a>
+      </div>
+</div>
+<div className="t-card">
+      <div className="t-image">
+        <Skeleton width={'100%'} height={'100%'}/>
+      </div>
+      <h4><Skeleton/></h4>
+      <p ><Skeleton/></p>
+      <div className="btnlearn">
+    <a><Skeleton/></a>
+      </div>
+</div>
+<div className="t-card">
+      <div className="t-image">
+        <Skeleton width={'100%'} height={'100%'}/>
+      </div>
+      <h4><Skeleton/></h4>
+      <p ><Skeleton/></p>
+      <div className="btnlearn">
+    <a><Skeleton/></a>
+      </div>
+</div>
+    </SkeletonTheme>
 
-      {tours.map((tour, index) => {
-            const { _id, name, price,image, description,summary } = tour;
-            return (
-              <TourData
-                key={_id}
-                image={image.filePath}
-                heading={name}
-                text={DOMPurify.sanitize(summary, {FORBID_TAGS: ['p', 'span']})}
-                url={`/tour-details/${_id}`}
-                button="learn more"
-              />
-            );
-          })}
+  
+) : (
+  tours.map((tour, index) => {
+    const { _id, name, price, image, description, summary } = tour;
+    return (
+      <TourData
+        key={_id}
+        image={image.filePath}
+        heading={name}
+        text={DOMPurify.sanitize(summary, { FORBID_TAGS: ['p', 'span'] })}
+        url={`/tour-details/${_id}`}
+        button="Learn More"
+      />
+    );
+  })
+)}
       </div>
     </div>
+    </SkeletonTheme>
   );
 }
 
